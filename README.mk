4645ffd8184c40f7870c098012e59eb910cf5785
Oliver <obryniarski@berkeley.edu>
2023-12-29 13:28:20 -0800

    fix: added readme

---
+++ README.mk
@@ -0,0 +1,10 @@
+# My dotfile setup
+
+## Installation
+
+```bash
+git clone --recursive-submodules git@github.com:obryniarski/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+chmod u+x install.sh
+sudo ./install.sh
+```

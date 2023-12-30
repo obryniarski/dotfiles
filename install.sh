13b53ac33dcdf76becf6737f0ccb25041244a885
Oliver <obryniarski@berkeley.edu>
2023-12-29 20:27:32 -0800

    fix: node version oof

--- install.sh
+++ install.sh
@@ -77,11 +77,11 @@ fi
 
 # install node (required for copilot in neovim)
 if test ! $(which node); then
-  echo $sudoPW | sudo -S apt-get install -y ca-certificates curl gnupg
+  echo $sudoPW | sudo -S apt-get install -y ca-certificates gnupg
   echo $sudoPW | sudo -S mkdir -p /etc/apt/keyrings
   curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | echo $sudoPW | sudo -S gpg --dearmor -o /etc/apt/keyrings/nodesource.gpgA
   NODE_MAJOR=20
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_14.x focal main" | echo $sudoPW | sudo -S tee /etc/apt/sources.list.d/nodesource.list
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | echo $sudoPW | sudo -S tee /etc/apt/sources.list.d/nodesource.list
   echo $sudoPW | sudo -S apt-get install -y nodejs
 fi
 

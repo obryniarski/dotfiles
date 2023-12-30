3ca959320a07fb8fc08041a3884b9747f4435946
Oliver <obryniarski@berkeley.edu>
2023-12-29 20:48:12 -0800

    fix: finally

--- install.sh
+++ install.sh
@@ -79,9 +79,8 @@ fi
 if test ! $(which node); then
   echo $sudoPW | sudo -S apt-get install -y ca-certificates gnupg
   echo $sudoPW | sudo -S mkdir -p /etc/apt/keyrings
-  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | echo $sudoPW | sudo -S gpg --dearmor -o /etc/apt/keyrings/nodesource.gpgA
-  NODE_MAJOR=20
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | echo $sudoPW | sudo -S tee /etc/apt/sources.list.d/nodesource.list
+  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpgA
+  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
   echo $sudoPW | sudo -S apt-get install -y nodejs
 fi
 

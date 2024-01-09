54851e4f81eb5b480473d37b1163ecf93169459b
Oliver <obryniarski@berkeley.edu>
2024-01-09 00:20:12 -0800

    testing out cmp and an lsp instead of copilot

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -1,31 +1,31 @@
 local plugins = {
 
-  {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    opts = function()
-      return require "custom.configs.copilot"
-    end,
-    config = function(_, opts)
-      require("copilot").setup(opts)
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   event = "InsertEnter",
+  --   opts = function()
+  --     return require "custom.configs.copilot"
+  --   end,
+  --   config = function(_, opts)
+  --     require("copilot").setup(opts)
+  --
+  --   vim.keymap.set("i", '<Tab>', function()
+  --     if require("copilot.suggestion").is_visible() then
+  --       require("copilot.suggestion").accept()
+  --     else
+  --       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+  --     end
+  --   end, {
+  --     silent = true,
+  --   })
+  --   end,
+  --
+  -- },
 
-    vim.keymap.set("i", '<Tab>', function()
-      if require("copilot.suggestion").is_visible() then
-        require("copilot.suggestion").accept()
-      else
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-      end
-    end, {
-      silent = true,
-    })
-    end,
-
-  },
-
-  {
-    "NvChad/nvim-cmp",
-    enabled = false,
-  },
+  -- {
+  --   "NvChad/nvim-cmp",
+  --   enabled = false,
+  -- },
 
   {
     "nvim-treesitter/nvim-treesitter",
@@ -50,7 +50,27 @@ local plugins = {
         "yaml",
       },
     }
-  }
+  },
+
+ {
+   "williamboman/mason.nvim",
+   opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "pyright",
+        "ruff",
+        "rust-analyzer",
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+     config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.configs.lspconfig"
+     end,
+  },
 
 }
 

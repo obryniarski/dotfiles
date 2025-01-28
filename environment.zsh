e91189603a9661399dac81c3fbd79bfe34ffc4cb
Oliver <obryniarski@berkeley.edu>
2025-01-28 15:57:28 -0800

    use nvim as default global editor

--- environment.zsh
+++ environment.zsh
@@ -1,2 +1,6 @@
-  export PATH=$PATH:/usr/local/go/bin
-  export PATH=$PATH:$HOME/go/bin
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/go/bin
+
+# use neovim as default global editor
+export VISUAL=nvim
+export EDITOR="$VISUAL"
